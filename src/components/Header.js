dec54df8ea2b24d64bb851b2117aaf242ae8ba9b
emmeiwhite <emmeiwhite@gmail.com>
2025-01-08 16:29:51 +0530

    portfolio-1.1 added the social media icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,43 @@
-import React, { useEffect, useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faLinkedin,
-  faMedium,
-  faStackOverflow,
-} from "@fortawesome/free-brands-svg-icons";
-import { Box, HStack } from "@chakra-ui/react";
+import React, { useEffect, useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faMedium, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { Box, HStack } from '@chakra-ui/react'
 
 const socials = [
   {
     icon: faEnvelope,
-    url: "mailto: hello@example.com",
+    url: 'mailto: hello@example.com'
   },
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: 'https://github.com'
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
+    url: 'https://www.linkedin.com'
   },
   {
     icon: faMedium,
-    url: "https://medium.com",
+    url: 'https://medium.com'
   },
   {
     icon: faStackOverflow,
-    url: "https://stackoverflow.com",
-  },
-];
+    url: 'https://stackoverflow.com'
+  }
+]
 
 const Header = () => {
-  const handleClick = (anchor) => () => {
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
+  const handleClick = anchor => () => {
+    const id = `${anchor}-section`
+    const element = document.getElementById(id)
     if (element) {
       element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+        behavior: 'smooth',
+        block: 'start'
+      })
     }
-  };
+  }
 
   return (
     <Box
@@ -54,26 +49,37 @@ const Header = () => {
       transitionProperty="transform"
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
+      backgroundColor="#18181b">
+      <Box
+        color="white"
+        maxWidth="1280px"
+        margin="0 auto">
         <HStack
           px={16}
           py={4}
           justifyContent="space-between"
-          alignItems="center"
-        >
+          alignItems="center">
           <nav>
             {/* Add social media links based on the `socials` data */}
+            <HStack spacing={8}>
+              {socials.map(link => {
+                return (
+                  <a href={link.url}>
+                    <FontAwesomeIcon
+                      icon={link.icon}
+                      size="2x"
+                    />
+                  </a>
+                )
+              })}
+            </HStack>
           </nav>
           <nav>
-            <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
-            </HStack>
+            <HStack spacing={8}>{/* Add links to Projects and Contact me section */}</HStack>
           </nav>
         </HStack>
       </Box>
     </Box>
-  );
-};
-export default Header;
+  )
+}
+export default Header
